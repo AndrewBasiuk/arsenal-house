e1a1d5768aeed6da5324ce9890022651c5d2ed0f
Andrew <latin601@gmail.com>
2018-03-16 16:02:10 +0200

    friday commit

--- dist/index.php
+++ dist/index.php
@@ -5,15 +5,98 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/style.min.css">
-    <title>Document</title>
+    <title>Arsenal House</title>
 </head>
 <body>
-    <header>Test</header>
+    <header>
+        <nav class="nav">
+            <ul class="wrapper">
+                <li class="nav__item">
+                    <a href="">
+                        <div class="nav__menu-btn">
+                            <div class="nav__menu-lines">
+                                <div class="nav__menu-line nav__menu-line_first"></div>
+                                <div class="nav__menu-line nav__menu-line_second"></div>
+                                <div class="nav__menu-line nav__menu-line_third"></div>
+                            </div>
+                            <div class="nav__menu-text">
+                                Menu
+                            </div>
+                        </div>
+                    </a>
+                </li>
+                <li class="nav__item">
+                    <a href="#">
+                        <div class="nav__icon nav__webcam_icon"></div>
+                    </a>
+                </li>
+                <li class="nav__item">
+                    <a href="">
+                        <div class="nav__icon nav__mail_icon"></div>
+                    </a>
+                </li>
+                <li class="nav__item"><a href="#">Main</a></li>
+                <li class="nav__item"><a href="#">About Complex</a></li>
+                <li class="nav__item"><a href="#">Choose appartment</a></li>
+                <li class="nav__item">
+                    <a href="#">
+                        <div class="nav__logo"></div>
+                    </a>
+                </li>
+                <li class="nav__item"><a href="#">Infrastructure</a></li>
+                <li class="nav__item"><a href="#">News</a></li>
+                <li class="nav__item"><a href="#">Contacts</a></li>
+                <li class="nav__item">
+                    <a href="#">
+                        <div class="nav__icon nav__map_icon"></div>
+                    </a>
+                </li>
+                <li class="nav__item language-select">
+                    <ul>
+                        <li><a href="#">UKR</a></li>
+                        <li><a href="#">RUS</a></li>
+                        <li><a href="#">EN</a></li>
+                    </ul>
+                    <div class="nav__icon nav__arrow_icon"></div>
+                </li>
+                <li class="nav__item nav__callback-btn"><a href="#">Callback</a></li>
+            </ul>
+        </nav>
+    </header>
     <section class="hero">
-        <div class="left"></div>
-        <div class="right"></div>
-        <div class="letter_a"></div>
+        <div class="hero__image-wrapper">
+            <div class="hero__image hero__image-left"></div>
+            <div class="hero__image hero__image-right"></div>
+        </div>
+        <div class="hero__letter-a">
+            <img src="img/main/letter_a.svg" alt="Letter A">
+        </div>
+        <div class="hero__main-text">
+            <p>Будинок для шанувальників розкішного життя</p>
+        </div>
+        <div class="hero__choose-apartment-btn">
+            <a href="#">Обрати апартаменти</a>
+        </div>
+        <div class="hero__next-view">
+            <div class="next-view next-view_brown">
+                <a href="#">Наступна <span class="next-view__icon next-view__icon_brown"></span>сторiнка</a>
+            </div>
+        </div>
+    </section>
+    <section class="building">
+        <div class="building__inner wrapper">
+            <div class="bulding__text-block section__text-block">
+                <div class="section__text-block_inner">
+                    <h3 class="section__text-block-heading">The Building</h3>
+                    <p>Lorem Ipsum - це текст-"риба", що використовується в друкарстві та дизайні. Lorem Ipsum є, фактично, 
+                        стандартною "рибою" аж з XVI сторіччя, коли невідомий друкар...
+                    </p>
+                    <a href="#" class="section__text-block_btn btn">Детальніше</a>
+                </div>
+            </div>
+        </div>
     </section>
     <script src="js/scripts.js"></script>
+    
 </body>
 </html>
