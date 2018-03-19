d07bf897e800c70bb5af1fcef494281504e3155f
Andrew <latin601@gmail.com>
2018-03-19 15:56:28 +0200

    Monday commit

--- dist/index.php
+++ dist/index.php
@@ -9,58 +9,62 @@
 </head>
 <body>
     <header>
-        <nav class="nav">
-            <ul class="wrapper">
-                <li class="nav__item">
-                    <a href="">
-                        <div class="nav__menu-btn">
-                            <div class="nav__menu-lines">
-                                <div class="nav__menu-line nav__menu-line_first"></div>
-                                <div class="nav__menu-line nav__menu-line_second"></div>
-                                <div class="nav__menu-line nav__menu-line_third"></div>
+        <nav class="nav-container">
+            <div class="wrapper nav">
+                <ul class="nav__left">
+                    <li class="nav__item">
+                        <a href="">
+                            <div class="nav__menu-btn">
+                                <div class="nav__menu-lines">
+                                    <div class="nav__menu-line nav__menu-line_first"></div>
+                                    <div class="nav__menu-line nav__menu-line_second"></div>
+                                    <div class="nav__menu-line nav__menu-line_third"></div>
+                                </div>
+                                <div class="nav__menu-text">
+                                    Menu
+                                </div>
                             </div>
-                            <div class="nav__menu-text">
-                                Menu
-                            </div>
-                        </div>
-                    </a>
-                </li>
-                <li class="nav__item">
-                    <a href="#">
-                        <div class="nav__icon nav__webcam_icon"></div>
-                    </a>
-                </li>
-                <li class="nav__item">
-                    <a href="">
-                        <div class="nav__icon nav__mail_icon"></div>
-                    </a>
-                </li>
-                <li class="nav__item"><a href="#">Main</a></li>
-                <li class="nav__item"><a href="#">About Complex</a></li>
-                <li class="nav__item"><a href="#">Choose appartment</a></li>
-                <li class="nav__item">
+                        </a>
+                    </li>
+                    <li class="nav__item">
+                        <a href="#">
+                            <div class="nav__icon nav__webcam_icon"></div>
+                        </a>
+                    </li>
+                    <li class="nav__item">
+                        <a href="">
+                            <div class="nav__icon nav__mail_icon"></div>
+                        </a>
+                    </li>
+                    <li class="nav__item"><a href="#">Main</a></li>
+                    <li class="nav__item"><a href="#">About Complex</a></li>
+                    <li class="nav__item"><a href="#">Choose appartment</a></li>
+                </ul>
+                <div class="nav__item nav__logo-container">
                     <a href="#">
                         <div class="nav__logo"></div>
                     </a>
-                </li>
-                <li class="nav__item"><a href="#">Infrastructure</a></li>
-                <li class="nav__item"><a href="#">News</a></li>
-                <li class="nav__item"><a href="#">Contacts</a></li>
-                <li class="nav__item">
-                    <a href="#">
-                        <div class="nav__icon nav__map_icon"></div>
-                    </a>
-                </li>
-                <li class="nav__item language-select">
-                    <ul>
-                        <li><a href="#">UKR</a></li>
-                        <li><a href="#">RUS</a></li>
-                        <li><a href="#">EN</a></li>
-                    </ul>
-                    <div class="nav__icon nav__arrow_icon"></div>
-                </li>
-                <li class="nav__item nav__callback-btn"><a href="#">Callback</a></li>
-            </ul>
+                </div>
+                <ul class="nav__right">
+                    <li class="nav__item"><a href="#">Infrastructure</a></li>
+                    <li class="nav__item"><a href="#">News</a></li>
+                    <li class="nav__item"><a href="#">Contacts</a></li>
+                    <li class="nav__item">
+                        <a href="#">
+                            <div class="nav__icon nav__map_icon"></div>
+                        </a>
+                    </li>
+                    <li class="nav__item language-select">
+                        <ul>
+                            <li><a href="#">UKR</a></li>
+                            <li><a href="#">RUS</a></li>
+                            <li><a href="#">EN</a></li>
+                        </ul>
+                        <div class="nav__icon nav__arrow_icon"></div>
+                    </li>
+                    <li class="nav__item nav__callback-btn"><a href="#">Callback</a></li>
+                </ul>
+            </div>
         </nav>
     </header>
     <section class="hero">
@@ -88,15 +92,191 @@
             <div class="bulding__text-block section__text-block">
                 <div class="section__text-block_inner">
                     <h3 class="section__text-block-heading">The Building</h3>
-                    <p>Lorem Ipsum - це текст-"риба", що використовується в друкарстві та дизайні. Lorem Ipsum є, фактично, 
+                    <p class="section__text-block-paragraph">Lorem Ipsum - це текст-"риба", що використовується в друкарстві та дизайні. Lorem Ipsum є, фактично, 
                         стандартною "рибою" аж з XVI сторіччя, коли невідомий друкар...
                     </p>
                     <a href="#" class="section__text-block_btn btn">Детальніше</a>
                 </div>
             </div>
+            <div class="building__next-view">
+                <div class="next-view next-view_black">
+                    <a href="#">Наступна <span class="next-view__icon next-view__icon_black"></span>сторiнка</a>
+                </div>
+            </div>
         </div>
     </section>
+    <section class="apartments">
+        <div class="apartments__inner wrapper">
+            <div class="apartments__text-block section__text-block">
+                <div class="section__text-block_inner">
+                    <h3 class="section__text-block-heading">Apartments</h3>
+                    <p class="section__text-block-paragraph">Lorem Ipsum - це текст-"риба", що використовується в друкарстві та дизайні. Lorem Ipsum є, фактично, 
+                        стандартною "рибою" аж з XVI сторіччя, коли невідомий друкар...
+                    </p>
+                    <a href="#" class="section__text-block_btn btn">Перейти до вибору</a>
+                </div>
+            </div>
+            <div class="apartments__next-view">
+                <div class="next-view next-view_brown">
+                    <a href="#">Наступна <span class="next-view__icon next-view__icon_brown"></span>сторiнка</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="infrastructure">
+        <div class="infrastructure__inner wrapper">
+            <div class="infrastructure__text-block section__text-block">
+                <div class="section__text-block_inner">
+                    <h3 class="section__text-block-heading">Infrastructure</h3>
+                    <p class="section__text-block-paragraph">Lorem Ipsum - це текст-"риба", що використовується в друкарстві та дизайні. Lorem Ipsum є, фактично, 
+                        стандартною "рибою" аж з XVI сторіччя, коли невідомий друкар...
+                    </p>
+                    <a href="#" class="section__text-block_btn btn">Детальніше</a>
+                </div>
+            </div>
+            <div class="infrastructure__next-view">
+                <div class="next-view next-view_black">
+                    <a href="#">Наступна <span class="next-view__icon next-view__icon_black"></span>сторiнка</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="quote">
+        <div class="quote__quote-text quote__text">
+            <h3 class="quote__text_heading">У тебе є вороги? Добре. Це означає, що в своєму житті ти щось відстоював.</h3>
+        </div>
+        <div class="quote__author">
+            <h4>Сер Уінстон Черчіль</h4>
+        </div>
+        <div class="quote__next-view">
+            <div class="next-view next-view_brown">
+                <a href="#">Наступна <span class="next-view__icon next-view__icon_brown"></span>сторiнка</a>
+            </div>
+        </div>
+    </section>
+    <section class="gallery">
+        <div>
+            <ul class="vertical-gallery" id="vertical-gallery">
+                <li data-thumb="img/main/gallery/thumbnails/1.jpg">
+                    <div class="gallery__item gallery__item_1"></div>
+                </li>
+            </ul>
+            <div class="gallery__thumbnails"></div>
+        </div>
+        <div class="gallery__text-block section__text-block">
+            <div class="section__text-block_inner">
+                <h3 class="section__text-block-heading">Location</h3>
+                <p class="section__text-block-paragraph">Lorem Ipsum - це текст-"риба", що використовується в друкарстві та дизайні. Lorem Ipsum є, фактично, 
+                    стандартною "рибою" аж з XVI сторіччя, коли невідомий друкар...
+                </p>
+                <a href="#" class="section__text-block_btn btn">Детальніше</a>
+            </div>
+        </div>
+        <div class="gallery__letter">
+            <img src="img/main/gallery_letter.svg" alt="Gallery letter">
+        </div>
+    </section>
+    <section class="quote quote2">
+        <div class="quote__quote-text quote__text">
+            <h3 class="quote__text_heading">У тебе є вороги? Добре. Це означає, що в своєму житті ти щось відстоював.</h3>
+        </div>
+        <div class="quote__author">
+            <h4>Сер Уінстон Черчіль</h4>
+        </div>
+        <div class="quote__next-view">
+            <div class="next-view next-view_brown">
+                <a href="#">Наступна <span class="next-view__icon next-view__icon_brown"></span>сторiнка</a>
+            </div>
+        </div>
+    </section>
+    <section class="news">
+        <h3 class="news_heading">
+            News
+        </h3>
+        <div class="wrapper news__wrapper">
+
+            <div class="news__item news__item_id-1">
+                <div class="news__item_inner-border">
+                    <div class="news__item_content">
+                        <h4 class="news__item_heading">Заголовок для нової новини у три рядки тексту</h4>
+                        <p class="news__item_paragraph">Lorem Ipsum - це текст-"риба", що використовується в 
+                            друкарстві та дизайні. Lorem Ipsum є, фактично, 
+                            стандартною "рибою"...
+                        </p>
+                        <a href="#" class="section__text-block_btn btn">Детальніше</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="news__item news__item_id-2">
+                <div class="news__item_inner-border">
+                    <div class="news__item_content">
+                        <h4 class="news__item_heading">Заголовок для нової новини у три рядки тексту</h4>
+                        <p class="news__item_paragraph">Lorem Ipsum - це текст-"риба", що використовується в 
+                            друкарстві та дизайні. Lorem Ipsum є, фактично, 
+                            стандартною "рибою"...
+                        </p>
+                        <a href="#" class="section__text-block_btn btn">Детальніше</a>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </section>
+    <section class="location">
+        <div class="location__wrapper">
+            <h3 class="location__heading">
+                Location
+            </h3>
+            <div class="contacts__info">
+                <ul>
+                    <li><span class="contacts__icon contacts__icon_main"></span><a href="#">Arsenalhouse@com.ua</a></li>
+                    <li><span class="contacts__icon contacts__icon_webcam"></span><a href="#">Веб -камера</a></li>
+                    <li><span class="contacts__icon contacts__icon_address"></span><a href="#">вул. Арсенальна 45</a></li>
+                    <li><span class="contacts__icon contacts__icon_phone"></span><a href="#">(044) 45-45-123</a></li>
+
+                </ul>
+            </div>
+            <p class="location__paragraph">
+                Вже давно відомо, що читабельний зміст буде заважати зосередитись людині, яка оцінює композицію сторінки. Сенс використання 
+                Lorem Ipsum полягає в тому, що цей текст має більш-менш нормальне розподілення літер на відміну від, наприклад, "Тут іде текст. 
+                Тут іде текст." Це робить текст схожим на оповідний.
+            </p>
+            <div class="location__form_outer">
+                <div class="location__form_inner">
+                    <form action="">
+                        <div>
+                            <div class="form__inputs form__inputs_top">
+                                <div class="input_container input__text">
+                                    <input name="name" type="text" class="input" placeholder="Ім'я:*">
+                                </div>
+                                <div class="input_container input__text" >
+                                    <input name="phone" type="text" class="input" placeholder="Телефон:*">
+                                </div>
+                                <div class="input_container input__text">
+                                    <input name="email" type="text" class="input" placeholder="E-mail:">
+                                </div>
+                            </div>
+                            <div class="form__inputs form__inputs_bottom">
+                                <div class="input_container input__textarea">
+                                    <textarea name="message" class="textarea" placeholder="Повідомлення:"></textarea>
+                                </div>
+                                <div class="input_container input__submit">
+                                    <input type="submit" class="submit" value="Надіслати">
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.slim.min.js"></script>
     <script src="js/scripts.js"></script>
+    <script>
+
+    </script>
     
 </body>
 </html>
