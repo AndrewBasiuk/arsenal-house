a1d63e7517850a98c0f4139f4f2a1ee8bd1089cc
Andrew <latin601@gmail.com>
2018-04-26 15:55:43 +0300

    Thursday commit

---
+++ dist/includes/header.php
@@ -0,0 +1,76 @@
+<header>
+        <nav class="nav-container">
+            <div class="wrapper nav">
+                <ul class="nav__left">
+                    <li class="nav__item">
+                        <a href="">
+                            <div class="nav__menu-btn">
+                                <div class="nav__menu-lines">
+                                    <div class="nav__menu-line nav__menu-line_first"></div>
+                                    <div class="nav__menu-line nav__menu-line_second"></div>
+                                    <div class="nav__menu-line nav__menu-line_third"></div>
+                                </div>
+                                <div class="nav__menu-text">
+                                    Menu
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+                    <li class="nav__item">
+                        <a href="#">
+                            <div class="nav__icon nav__webcam_icon nav__icon_desktop"></div>
+                        </a>
+                    </li>
+                    <li class="nav__item">
+                        <a href="">
+                            <div class="nav__icon nav__mail_icon nav__icon_desktop"></div>
+                        </a>
+                    </li>
+                    <li class="nav__item nav__item_desk"><a href="#">Головна</a></li>
+                    <li class="nav__item nav__item_desk"><a href="#">Про комплекс</a></li>
+                    <li class="nav__item nav__item_desk"><a href="#">Обрати кваритру</a></li>
+                </ul>
+                <div class="nav__item nav__logo-container">
+                    <a href="#">
+                        <div class="nav__logo"></div>
+                    </a>
+                </div>
+                <ul class="nav__right">
+                    <li class="nav__item nav__item_desk"><a href="#">Інфраструктура</a></li>
+                    <li class="nav__item nav__item_desk"><a href="#">Новини</a></li>
+                    <li class="nav__item nav__item_desk"><a href="#">Контакти</a></li>
+                    <li class="nav__item">
+                        <a href="#">
+                            <div class="nav__icon nav__map_icon"></div>
+                        </a>
+                    </li>
+                    <li class="nav__item language-select">
+                        <ul>
+                            <li><a href="#">UKR</a></li>
+                            <li><a href="#">RUS</a></li>
+                            <li><a href="#">EN</a></li>
+                        </ul>
+                        <div class="nav__icon nav__arrow_icon"></div>
+                    </li>
+                    <li class="nav__item">
+                        <a href="#">
+                            <div class="nav__icon nav__webcam_icon nav__icon_mobile"></div>
+                        </a>
+                    </li>
+                    <li class="nav__item">
+                        <a href="">
+                            <div class="nav__icon nav__mail_icon nav__icon_mobile"></div>
+                        </a>
+                    </li>
+                    <li class="nav__item nav__callback-btn">
+                        <a href="#">Зворотній дзвінок</a>
+                    </li>
+                    <li class="nav__item nav__callback-btn_mobile">
+                        <a href="">
+                            <div class="nav__icon nav__phone_icon">
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
--- dist/index.php
+++ dist/index.php
@@ -9,82 +9,7 @@
 </head>
 <body>
     <?php include_once('includes/preloader.php'); ?>
-    <header>
-        <nav class="nav-container">
-            <div class="wrapper nav">
-                <ul class="nav__left">
-                    <li class="nav__item">
-                        <a href="">
-                            <div class="nav__menu-btn">
-                                <div class="nav__menu-lines">
-                                    <div class="nav__menu-line nav__menu-line_first"></div>
-                                    <div class="nav__menu-line nav__menu-line_second"></div>
-                                    <div class="nav__menu-line nav__menu-line_third"></div>
-                                </div>
-                                <div class="nav__menu-text">
-                                    Menu
-                                </div>
-                            </div>
-                        </a>
-                    </li>
-                    <li class="nav__item">
-                        <a href="#">
-                            <div class="nav__icon nav__webcam_icon nav__icon_desktop"></div>
-                        </a>
-                    </li>
-                    <li class="nav__item">
-                        <a href="">
-                            <div class="nav__icon nav__mail_icon nav__icon_desktop"></div>
-                        </a>
-                    </li>
-                    <li class="nav__item nav__item_desk"><a href="#">Головна</a></li>
-                    <li class="nav__item nav__item_desk"><a href="#">Про комплекс</a></li>
-                    <li class="nav__item nav__item_desk"><a href="#">Обрати кваритру</a></li>
-                </ul>
-                <div class="nav__item nav__logo-container">
-                    <a href="#">
-                        <div class="nav__logo"></div>
-                    </a>
-                </div>
-                <ul class="nav__right">
-                    <li class="nav__item nav__item_desk"><a href="#">Інфраструктура</a></li>
-                    <li class="nav__item nav__item_desk"><a href="#">Новини</a></li>
-                    <li class="nav__item nav__item_desk"><a href="#">Контакти</a></li>
-                    <li class="nav__item">
-                        <a href="#">
-                            <div class="nav__icon nav__map_icon"></div>
-                        </a>
-                    </li>
-                    <li class="nav__item language-select">
-                        <ul>
-                            <li><a href="#">UKR</a></li>
-                            <li><a href="#">RUS</a></li>
-                            <li><a href="#">EN</a></li>
-                        </ul>
-                        <div class="nav__icon nav__arrow_icon"></div>
-                    </li>
-                    <li class="nav__item">
-                        <a href="#">
-                            <div class="nav__icon nav__webcam_icon nav__icon_mobile"></div>
-                        </a>
-                    </li>
-                    <li class="nav__item">
-                        <a href="">
-                            <div class="nav__icon nav__mail_icon nav__icon_mobile"></div>
-                        </a>
-                    </li>
-                    <li class="nav__item nav__callback-btn">
-                        <a href="#">Зворотній дзвінок</a>
-                    </li>
-                    <li class="nav__item nav__callback-btn_mobile">
-                        <a href="">
-                            <div class="nav__icon nav__phone_icon">
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    </header>
+    <?php include_once('includes/header.php'); ?>
 
     <section class="hero">
         <div class="hero__image-wrapper">
@@ -108,19 +33,25 @@
     </section>
 
     <section class="building">
-    <canvas class="building__canvas" width="1800" height="872"></canvas>
+        <canvas class="building__canvas" width="1800" height="872"></canvas>
         <div class="building__inner wrapper">
-            <div class="section__text-block_outer bulding__text-block">
-                <div class="section__text-block">
-                    <div class="section__text-block_inner">
-                        <h3 class="section__text-block-heading">The Building</h3>
-                        <p class="section__text-block-paragraph">Lorem Ipsum - це текст-"риба", що використовується в друкарстві та дизайні. Lorem Ipsum є, фактично, 
-                            стандартною "рибою" аж з XVI сторіччя, коли невідомий друкар...
-                        </p>
-                        <a href="#" class="section__text-block_btn btn">Детальніше</a>
+            <div class="section__box_buiding">
+                <div class="section__box_outer">
+                    <div class="section__box_inner">
+                        <div class="section__box-text-container">
+                            <div>
+                                <h4 class="section__box-heading">The Building</h4>
+                            </div>
+                            <div class="section__box-paragraphs">
+                                <p class="section__box-paragraph">Часи змінюються, але історичний центр поблизу Парламенту та набережної Дніпра залишається найбільш респектабельним місцем для проживання.</p>
+                                <p class="section__box-paragraph">Ласкаво просимо до Arsenal House - ідеальне місце для тих, хто завжди знаходиться в центрі подій, але при цьому цінує тишу та спокій! Горда архітектура фасадів з ювелірною точністю вписана в самобутній район Печерська, де до сих пір зберігся дух старого Києва.</p>
+                                <p class="section__box-paragraph">Арсенал справжніх асоціацій, атмосфера та стан душі</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
+
             <div class="building__next-view">
                 <div class="next-view next-view_black">
                     <a href="#" data-nextView=".apartments">Наступна <span class="next-view__icon next-view__icon_black"></span>сторiнка</a>
